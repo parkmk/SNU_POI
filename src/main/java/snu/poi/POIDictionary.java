d2098101e47091ee720bb1d00c1a385e91ee49db
kijungshin <kijung.shin89@gmail.com>
2015-06-12 18:14:50 +0900

    add additional dictionary

--- src/main/java/snu/poi/POIDictionary.java
+++ src/main/java/snu/poi/POIDictionary.java
@@ -1,16 +1,18 @@
 package snu.poi;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class POIDictionary {
 
     private static POIDictionary dict;
     private Map<String, Character> typeMap = new HashMap ();
     private String[] words;
+
+    private final static Pattern parenthesisPattern = Pattern.compile("(.*)(\\s*)\\((.*)\\)");
+    private final static Pattern branchPattern =  Pattern.compile("(.*)(\\s+)(.*)점$");
 
     private  POIDictionary() throws IOException {
         load();
@@ -43,17 +45,31 @@ public class POIDictionary {
             if (line==null) {
                 break;
             }
-            String[] tokens = line.trim().replaceAll(" ","").split("\t");
-            String entry = tokens[0];
+            String[] tokens = line.trim().split("\t");
+            List<String> entryList = new LinkedList<String>();
+            entryList.add(tokens[0].replaceAll(" ", ""));
+
+            Matcher m = parenthesisPattern.matcher(tokens[0]);
+            if(m.find()) {
+                entryList.add(m.group(1).replaceAll(" ", ""));
+//                entryList.add(m.group(3).replaceAll(" ", ""));
+            }
+
+            m = branchPattern.matcher(tokens[0]);
+            if(m.find()) {
+                entryList.add(m.group(1).replaceAll(" ", ""));
+            }
+
             char type = tokens[1].charAt(0);
-            if(!excludeSet.contains(entry) && entry.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*") && entry.length() >= 3) {
-                if (typeMap.containsKey(entry)) {
-                    if (type!='U') { //if type is known
+            for(String entry : entryList) {
+                if (!excludeSet.contains(entry) && entry.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*") && entry.length() >= 3) {
+                    if (typeMap.containsKey(entry)) {
+                        if (type != 'U') { //if type is known
+                            typeMap.put(entry, type);
+                        }
+                    } else {
                         typeMap.put(entry, type);
                     }
-                }
-                else {
-                    typeMap.put(entry, type);
                 }
             }
         }
